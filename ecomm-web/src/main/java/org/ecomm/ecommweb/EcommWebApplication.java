04b81fc7028070b44bdea7039f7417d23bd4392c
Amol Singh <amolsingh9372@gmail.com>
2023-12-18 01:36:15 +0000

    sample

---
+++ ecomm-web/src/main/java/org/ecomm/ecommweb/EcommWebApplication.java
@@ -0,0 +1,13 @@
+package org.ecomm.ecommweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommWebApplication.class, args);
+	}
+
+}
---
+++ ecomm-web/src/test/java/org/ecomm/ecommweb/EcommWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecomm.ecommweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
