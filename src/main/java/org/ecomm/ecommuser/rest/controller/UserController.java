1a63842997e2f23650990ee33526062bd08e05f4
siuuuu <amolsingh9372@gmail.com>
2024-01-02 12:22:14 -0500

    user microservice

--- src/main/java/org/ecomm/ecommuser/rest/controller/UserController.java
+++ src/main/java/org/ecomm/ecommuser/rest/controller/UserController.java
@@ -3,7 +3,8 @@ package org.ecomm.ecommuser.rest.controller;
 import java.net.URI;
 import java.util.Map;
 
-import org.ecomm.ecommuser.rest.model.User;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.ecomm.ecommuser.rest.request.AddUserRequest;
 import org.ecomm.ecommuser.rest.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,16 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
+@Slf4j
 public class UserController {
 
   @Autowired UserService userService;
 
   @PostMapping
-  public ResponseEntity<Object> createAppUser(@RequestBody AddUserRequest request) {
+  public ResponseEntity<Object> createAppUser(@RequestBody AddUserRequest request, HttpServletRequest httpServletRequest) {
     var user = userService.createAppUser(request);
+
+    log.info("Request is ::: {}", httpServletRequest.toString());
 
     return ResponseEntity.created(URI.create("/user")).body(Map.of("id", user.getId()));
   }
