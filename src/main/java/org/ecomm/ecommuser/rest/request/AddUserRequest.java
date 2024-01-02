dacf6260a7b18f041ae0e6fe41d32aca29d3413b
siuuuu <amolsingh9372@gmail.com>
2024-01-02 09:35:48 -0500

    user microservice

--- src/main/java/org/ecomm/ecommuser/rest/request/AddUserRequest.java
+++ src/main/java/org/ecomm/ecommuser/rest/request/AddUserRequest.java
@@ -3,12 +3,14 @@ package org.ecomm.ecommuser.rest.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @Setter
 @Getter
 @SuperBuilder(toBuilder = true)
 @NoArgsConstructor
+@ToString
 public class AddUserRequest {
 
     String firstName;
--- src/main/java/org/ecomm/ecommuser/rest/services/UserServiceImpl.java
+++ src/main/java/org/ecomm/ecommuser/rest/services/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package org.ecomm.ecommuser.rest.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.ecomm.ecommuser.persistance.entity.user.EUser;
 import org.ecomm.ecommuser.persistance.repository.UserRepository;
 import org.ecomm.ecommuser.rest.builder.UserBuilder;
@@ -9,12 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
 
   @Autowired UserRepository userRepository;
 
   @Override
   public User createAppUser(AddUserRequest request) {
+
+    log.info("Incoming request ::: {}", request.toString());
 
     EUser eUser = UserBuilder.with(request);
     EUser savedUser = userRepository.save(eUser);
